b7daa670a2d2e354fb16b417639ea04620b58dba
ishu22ji <78218217+ishu22ji@users.noreply.github.com>
2021-02-09 04:54:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const mineflayer = require('mineflayer')
 
-if (process.argv.length < 4 || process.argv.length > 6) {
-    console.log('Usage : node reconnector.js <host> <port> [<name>]')
-    process.exit(1)
-}
 function createBot () {
     const bot = mineflayer.createBot({
-        host: process.argv[2],
-        port: parseInt(process.argv[3]),
-        username: process.argv[4] ? process.argv[4] : 'hello',
+        host: "ishuserver.aternos.me",
+        port: 59723,
+        username: 'hello',
     })
     bot.on('error', (err) => console.log(err))
     bot.on('end', () => setTimeout(createBot, 10000))
