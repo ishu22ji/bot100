6e45a863a42d0a836ea5365ab81a023a1fe4fed2
ishu22ji <78218217+ishu22ji@users.noreply.github.com>
2021-05-16 01:24:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,136 @@
 const mineflayer = require('mineflayer')
+const pvp = require('mineflayer-pvp').plugin
+const { pathfinder, Movements, goals} = require('mineflayer-pathfinder')
+const armorManager = require('mineflayer-armor-manager')
+const GoalFollow = goals.GoalFollow
 
 function createBot () {
     const bot = mineflayer.createBot({
-        host: "ishuserver.aternos.me",
-        port: 59723,
+        host: "ishukaserver.aternos.me",
+        port: 28193,
         username: 'hello',
     })
     bot.on('error', (err) => console.log(err))
     bot.on('end', () => setTimeout(createBot, 10000))
   
-    function login() {
-      bot.chat("/login qwer")
-      bot.chat("/tp hello -337 69 -354")
-      bot.chat("hlo ishu")
-    }
-  
-    bot.once('spawn', login)
+bot.loadPlugin(pvp)
+bot.loadPlugin(armorManager)
+bot.loadPlugin(pathfinder)
+
+
+bot.on('playerCollect', (collector) => {
+  if (collector !== bot.entity) return
+
+  setTimeout(() => {
+    const sword = bot.inventory.items().find(item => item.name.includes('sword'))
+    if (sword) bot.equip(sword, 'hand')
+  }, 150)
+})
+
+bot.on('playerCollect', (collector, itemDrop) => {
+  if (collector !== bot.entity) return
+
+  setTimeout(() => {
+    const shield = bot.inventory.items().find(item => item.name.includes('shield'))
+    if (shield) bot.equip(shield, 'off-hand')
+  }, 250)
+})
+
+let guardPos = null
+
+function guardArea (pos) {
+  guardPos = pos.clone()
+
+  if (!bot.pvp.target) {
+    moveToGuardPos()
+  }
 }
-createBot()
+
+function stopGuarding () {
+  guardPos = null
+  bot.pvp.stop()
+  bot.pathfinder.setGoal(null)
+}
+
+function moveToGuardPos () {
+  const mcData = require('minecraft-data')(bot.version)
+  bot.pathfinder.setMovements(new Movements(bot,mcData))
+  bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
+}
+function followPlayer() {
+    const playerCI = bot.players['ishubro']
+
+    if (!playerCI || !playerCI.entity) {
+        bot.chat("I can't see you ishubro!")
+        return
+    }
+
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = []
+
+    bot.pathfinder.setMovements(movements)
+
+    const goal = new GoalFollow(playerCI.entity, 1)
+    bot.pathfinder.setGoal(goal, true)
+}
+
+bot.on('stoppedAttacking', () => {
+  if (guardPos) {
+    moveToGuardPos()
+  }
+})
+
+bot.on('physicTick', () => {
+  if (bot.pvp.target) return
+  if (bot.pathfinder.isMoving()) return
+
+  const entity = bot.nearestEntity()
+  if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0))
+})
+
+bot.on('physicTick', () => {
+  if (!guardPos) return
+
+  const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16 &&
+                      e.mobType !== 'Armor Stand' // Mojang classifies armor stands as mobs for some reason?
+
+  const entity = bot.nearestEntity(filter)
+  if (entity) {
+    bot.pvp.attack(entity)
+  }
+})
+
+bot.on('chat', (username, message) => {
+  if (message === 'guard') {
+    const player = bot.players[username]
+
+    if (!player) {
+      bot.chat("I can't see you.")
+      return
+    }
+  if (message === 'follow') {
+    followPlayer()
+    }
+
+    bot.chat('I will guard that location.')
+    guardArea(player.entity.position)
+  }
+
+  if (message === 'fight me') {
+    const player = bot.players[username]
+
+    if (!player) {
+      bot.chat("I can't see you.")
+      return
+    }
+
+    bot.chat('Prepare to fight!')
+    bot.pvp.attack(player.entity)
+  }
+
+  if (message === 'stop') {
+    bot.chat('I will no longer guard this area')
+    stopGuarding()
+  }
+})
